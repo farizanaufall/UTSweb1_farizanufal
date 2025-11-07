9c16a888676e14ad09167f93c0d50a2e8d7ef7f1
farizanaufall <farizanaufal71@gmail.com>
2025-11-07 16:44:43 +0700

    logika penjualan

--- dashboard.php
+++ dashboard.php
@@ -10,6 +10,21 @@ $kode_barang = ["BRG001", "BRG002", "BRG003", "BRG004", "BRG005"];
 $nama_barang = ["Beras", "Gula", "popmie", "aqua", "Kopi"];
 $harga_barang = [15000, 12000, 11000, 5000, 8000];
 
+$beli = [];
+$jumlah = [];
+$total = [];
+$grandtotal = 0;
+
+$jumlah_item = rand(1, 5);
+for ($i = 0; $i < $jumlah_item; $i++) {
+    $index = rand(0, 4);
+    $beli[] = $nama_barang[$index];
+    $jumlah[] = rand(1, 10); 
+    $total[] = $harga_barang[$index] * $jumlah[$i];
+    $grandtotal += $total[$i];
+}
+
+
 ?>
 
 <!DOCTYPE html>
